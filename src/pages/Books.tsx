49ce715af0fdbad242d879997c2b946f4b2ebff0
Dricc-new <diegoramoncc17@gmail.com>
2023-08-07 22:31:46 -0400

    migrating form to formik

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import AppLayout from "../layouts/AppLayout";
 import 'react-router-dom';
-
+import * as Yup from 'yup'
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import { withRouter, WithRouterProps } from '../withRouter';
 import { createBook, getBook, updateBook } from "../services/BookServices";
 
@@ -9,9 +10,7 @@ interface Params {
     id: string;
 }
 
-type Props = WithRouterProps<Params>;
-
-class Books extends Component<Props> {
+class Books extends Component<WithRouterProps<Params>> {
     state = {
         book: { title: '', author: '', description: '' },
         id: ''
@@ -38,52 +37,66 @@ class Books extends Component<Props> {
         }
     }
 
-    // On Submit
-    onSubmit = async (e: any) => {
-        e.preventDefault()
-        try {
-            const newBook = {
-                title: this.state.book.title,
-                author: this.state.book.author,
-                description: this.state.book.description,
-            }
-
-            // If the id exists, update the book; otherwise create a new book
-            if (this.state.id) {
-                await updateBook(this.state.id, newBook)
-            } else {
-                await createBook(newBook)
-            }
-
-            // redirect to dashboard
-            window.location.href = '/dashboard '
-        } catch (err) {
-            console.log(err)
-        }
-    }
-
     render = (): React.ReactNode => <AppLayout>
         <section className="flex justify-center font-bold text-stone-700">
-            <form onSubmit={this.onSubmit} className="flex rounded gap-4 m-4 bg-white shadow-sm shadow-slate-900 p-4 items-center flex-col">
-                <h2 className="text-2xl text-color-primary">{this.state.id ? 'Edit' : 'New'} Book</h2>
-                <div className="flex gap-4 max-md:flex-col">
-                    <div className="flex flex-col justify-between">
-                        <article className="flex gap-2 justify-start flex-col">
-                            <label htmlFor="titleBook" className="decoration-sky-600 underline decoration-2">Title</label>
-                            <input required value={this.state.book.title} id="titleBook" onChange={this.onChange} name="title" type="text" className="border p-2 focus:outline-blue-900 border-blue-900 rounded" />
-                        </article>
-                        <article className="flex gap-2 justify-start flex-col">
-                            <label htmlFor="AuthorBook" className="decoration-sky-600 underline decoration-2">Author</label>
-                            <input required value={this.state.book.author} id="AuthorBook" onChange={this.onChange} name="author" type="text" className="border p-2 focus:outline-blue-900 border-blue-900 rounded" />
-                        </article>
+            <Formik
+                initialValues={{
+                    title: '',
+                    author: '',
+                    description: ''
+                }}
+
+                validationSchema={Yup.object({
+                    title: Yup.string().required('Title is required'),
+                    author: Yup.string().required('Author is required'),
+                    description: Yup.string().required('Description is required')
+                })}
+
+                onSubmit={async (values, actions) => {
+                    try {
+                        // If the id exists, update the book; otherwise create a new book
+                        if (this.state.id) {
+                            await updateBook(this.state.id, values)
+                        } else {
+                            await createBook(values)
+                        }
+                        // redirect to dashboard
+                        const { navigate } = this.props;
+                        navigate('/dashboard')
+                    } catch (err) {
+                        console.log(err)
+                    }
+                }}>
+
+                <Form className="flex rounded gap-4 m-4 bg-white shadow-sm shadow-slate-900 p-4 items-center flex-col">
+                    <h2 className="text-2xl text-color-primary">{this.state.id ? 'Edit' : 'New'} Book</h2>
+
+                    <div className="flex gap-4 max-md:flex-col">
+                        <div className="flex flex-col justify-between">
+
+                            <div className="flex gap-2 justify-start flex-col">
+                                <label htmlFor="titleBook">Title</label>
+                                <Field id="titleBook" name='title'></Field>
+                                <ErrorMessage component="p" className="input-error" name='title' />
+                            </div>
+
+                            <div className="flex gap-2 justify-start flex-col">
+                                <label htmlFor="AuthorBook">Author</label>
+                                <Field id="AuthorBook" name='author'></Field>
+                                <ErrorMessage component="p" className="input-error" name='author' />
+                            </div>
+                        </div>
+
+                        <div className="flex gap-2 justify-start flex-col">
+                            <label htmlFor="descBook">Description</label>
+                            <Field component="textarea" cols={30} rows={5} id="descBook" name='description'></Field>
+                            <ErrorMessage component="p" className="input-error" name='description' />
+                        </div>
                     </div>
-                    <article className="flex gap-2 justify-start flex-col">
-                        <label htmlFor="descBook" className="decoration-sky-600 underline decoration-2">Description</label>
-                        <textarea required value={this.state.book.description} id="descBook" onChange={this.onChange} name="description" cols={30} rows={5} className="border p-2 focus:outline-blue-900 border-blue-900 rounded" ></textarea>
-                    </article>
-                </div>
-                <button disabled={!(this.state.book.title && this.state.book.description && this.state.book.author)} type="submit" className="btn-primary w-full h-10">Submit</button>
-            </form>
+
+                    <button type="submit" className="btn-primary w-full h-10">Submit</button>
+                </Form>
+            </Formik>
         </section>
     </AppLayout>
 }
